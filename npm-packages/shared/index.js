a24c1d1e6663f26551926ec52bafea2f5dc77251
Nigel Todman <veritas@vts-tech.org>
2026-04-09 18:25:06 -0400

    1.0.3

---
+++ npm-packages/shared/index.js
@@ -0,0 +1,5 @@
+// Re-exports for convenience
+module.exports.format = require("./format");
+module.exports.ollama = require("./ollama");
+module.exports.security = require("./security");
+module.exports.types = require("./types");
